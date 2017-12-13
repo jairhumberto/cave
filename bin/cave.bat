c285acbb02849d53c4aa8c497ce55ce12b6bbbb0
Humberto <jairhumberto@gmail.com>
2017-12-13 02:39:21 -0200

    Corrections on the bat file

--- bin/cave.bat
+++ bin/cave.bat
@@ -1,41 +1,23 @@
 @echo off
-
-rem This script will do the following:
-rem - check for PHP_COMMAND env, if found, use it.
-rem   - if not found detect php, if found use it, otherwise err and terminate
-
 if "%OS%"=="Windows_NT" @setlocal
 
-rem %~dp0 is expanded pathname of the current script under NT
-set DEFAULT_CAVE_HOME=%~dp0..
-
-goto init
-goto cleanup
-
 :init
+if "%PHP_COMMAND%" == "" goto set_php_command
 
-if "%CAVE_HOME%" == "" set CAVE_HOME=%DEFAULT_CAVE_HOME%
-set DEFAULT_CAVE_HOME=
+REM %~dp0 is the pathname of the current script under NT
+if "%CAVE_HOME%" == "" set CAVE_HOME=%~dp0..
 
-if "%PHP_COMMAND%" == "" goto no_phpcommand
-
-goto run
-goto cleanup
-
-:run
 "%PHP_COMMAND%" -d html_errors=off -qC "%CAVE_HOME%\bin\cave" %*
 goto cleanup
 
-:no_phpcommand
-rem PHP_COMMAND environment variable not found, assuming php.exe is on path.
+:set_php_command
+REM PHP_COMMAND environment variable not found, assuming php.exe is on path.
 set PHP_COMMAND=php.exe
 goto init
 
 :err_home
 echo ERROR: Environment var CAVE_HOME not set. Please point this
 echo variable to your local cave installation!
-goto cleanup
 
 :cleanup
 if "%OS%"=="Windows_NT" @endlocal
-rem pause
